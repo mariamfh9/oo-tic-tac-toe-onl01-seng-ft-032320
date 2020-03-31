fafac13ac46d7dbd0151bb472f05916f2f60cfff
Mariam Haider <mfh.haider@gmail.com>
2020-03-31 04:42:01 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -126,6 +126,16 @@ def winner
   end
 end
 
+def play
+  until over? == true
+    turn
+  end
 
+  if won?
+    puts "Congratulations #{winner}!"
+  elsif draw?
+    puts "Cat's Game!"
+  end
+end
 
 end
